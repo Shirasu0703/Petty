dc7d797b2c199a72d62105330802be172ecaf5d9
Shirasu0703 <219084816+Shirasu0703@users.noreply.github.com>
2025-10-02 14:19:13 +0000

    [Add]top.aboutページ

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def mypage
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def publish_unpublish
+  end
+
+  def unsubcribe
+  end
+
+  def withdraw
+  end
+end
